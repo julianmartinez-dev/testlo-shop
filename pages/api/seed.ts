7da9f4392598646125d64321feaa690d35f22aa7
Julian Martinez <julian.fmartinez@gmail.com>
2022-06-17 16:46:20 -0300

    add: Se conecta con mongodb

---
+++ pages/api/seed.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db, seedDB } from '../../database'
+import { Product } from '../../models'
+
+type Data = {
+    message: string
+}
+
+export default async function (req: NextApiRequest, res: NextApiResponse<Data>) {
+    if(process.env.NODE_ENV === 'production'){
+        return res.status(401).json({
+            message: 'No se puede ejecutar esta acción en producción'
+        })
+    }
+
+    await db.connect()
+    await Product.deleteMany();
+    await Product.insertMany(seedDB.initialData.products);
+    await db.disconnect();
+
+    res.status(200).json({
+        message: 'Base de datos inicializada'
+    })
+}
